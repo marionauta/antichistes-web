93769335dc7b89774353d13f9d7f5c0409ff3bac
Mario Nachbaur <marionauta@users.noreply.github.com>
2017-08-27 20:40:12 +0200

    Better web app
    - Closer to PWA.
    - Clean look.
    - Write in TypeScript.
    - Add a web manifest file.

--- app/js/antichistes.js
+++ app/js/antichistes.js
@@ -1,108 +1,46 @@
-var id;
-var recuentos = 0;
-var p = 0;
-var version = '1.4.5';
-var urlbase = 'https://get.eldiaque.tk/antichistes/api.php?version=' + version;
-
-function ocultarExcepto(sec) {
-	$('.main').hide();
-	$(sec).show();
+var version = '2.0.0';
+var urlbase = 'https://get.eldiaque.tk/antichistes';
+var first_part = document.getElementById('first-part');
+var second_part = document.getElementById('second-part');
+var button_next = document.getElementById('button-next');
+var button_like = document.getElementById('button-like');
+var button_show = document.getElementById('button-show');
+var current = null;
+function random() {
+    var req = new XMLHttpRequest();
+    req.open('GET', urlbase + "/random/one", true);
+    req.onload = function (e) {
+        if (req.readyState === 4 && req.status === 200) {
+            current = JSON.parse(req.responseText).items[0];
+            first_part.textContent = current.first_part;
+            second_part.textContent = current.second_part;
+            first_part.style.display = 'block';
+        }
+    };
+    req.send(null);
 }
-function parametro() {
-    return Math.floor(Math.random()*100000);
-}
-
 function aleatorio() {
-	ocultarExcepto('#aleatorio');
-    $('.opt, #an').hide();
-    $('#ap').text('...');
-    $.getJSON(urlbase, {method: 'aleatorio', pp: parametro()},
-        function(data) {
-            if (data.error != 0) {
-                $('#ap').text(data.message);
-            } else {
-                $('#ap').text(data.uno);
-                $('#an').text(data.dos);
-                $('.see').show();
-                id = data.cero;
-                recuentos += 1;
-            }
-        }
-    );
+    button_show.style.display = 'block';
+    [first_part, second_part, button_like, button_next].forEach(function (b) {
+        b.style.display = 'none';
+    });
+    random();
 }
-
-function ultimos() {
-    p += 1;
-    $('#ultibot').removeClass('more').text('...').show();
-    $.getJSON(urlbase, {method: 'ultimos', p:p, pp:parametro()},
-        function(data) {
-            if (data.error != 0) {
-                $('.more').removeClass('more').text(data.message).show();
-            } else {
-                $.each(data.cola, function(i, value) {
-                    var insert = '<div class="cont see"><div class="parece">'+ value.uno + '</div><div class="noes">' + value.dos +'</div></div>';
-                    $('#list').append(insert);
-                    $('#ultibot').addClass('more').text('Cargar más');
-                });
-            }
-        }
-    );
+function like() {
+    button_like.style.display = 'none';
+    var req = new XMLHttpRequest();
+    req.open('POST', urlbase + "/vote", true);
+    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    req.send("id=" + current.id);
 }
-
-$(function() { // cuando document ready (jQuery)
-	FastClick.attach(document.body);
-    console.log('AntiChistes v' + version);
-
-    /* Sólo cambiar de sección si no está activa */
-    $('nav div').click(function() {
-        if (!$(this).hasClass('visto')) {
-            $('nav div').removeClass('visto');
-            $(this).addClass('visto');
-
-            if ($(this).hasClass('aleatorio')) { aleatorio() }
-			else if ($(this).hasClass('enviar')) { alert('Not implemented') }
-            else if ($(this).hasClass('ultimos')) {
-                if (p === 0) { ultimos() } // Llamar a la función si nunca se ha llamado antes.
-
-                ocultarExcepto('#ultimos');
-                $('#ultibot').show();
-            	window.scroll(0, document.body.scrollHeight);
-            }
-        }
+function show() {
+    button_show.style.display = 'none';
+    [second_part, button_like, button_next].forEach(function (b) {
+        b.style.display = 'block';
     });
-
-    $(document).on('click', '.see', function() {
-        /* Si está en últimos */
-        if ($(this).children('.noes').text()) {
-            $(this).children('.noes').slideDown(200);
-            $(this).removeClass('see').addClass('send');
-
-        /* Si está en aleatorio */
-        } else {
-            $('#an').slideDown(200);
-            $('.opt').show();
-            $(this).hide();
-
-        }
-    });
-    $(document).on('click', '.send', function() {
-        /* Si está en últimos */
-        if ($(this).children('.parece').text()) {
-            var texto = $(this).children('.parece').text() + ' ' + $(this).children('.noes').text();
-
-        /* Si está en aleatorio */
-        } else {
-            var texto = $('#ap').text() +' '+ $('#an').text();
-
-        }
-        console.log('Compartido: ' + texto);
-		Android.sendShare(texto + ' http://antichistes.ga')
-    });
-    $('.otro').click(function() { aleatorio() } );
-    $('.more').click(function() { ultimos() });
-    $('.vote').click(function() {
-        console.log('Gustado: ' + id);
-        $.get(urlbase, {method:'plusone', id:id});
-        $(this).slideUp();
-    });
-});
+}
+function main() {
+    console.log("AntiChistes v" + version);
+    console.log('Mira el código en https://github.com/marionauta/antichistes');
+    aleatorio();
+}
---
+++ app/ts/antichistes.ts
@@ -0,0 +1,72 @@
+interface AntiJoke {
+    id: Number,
+    first_part: string,
+    second_part: string,
+}
+
+const version = '2.0.0';
+const urlbase = 'https://get.eldiaque.tk/antichistes';
+
+const first_part = document.getElementById('first-part');
+const second_part = document.getElementById('second-part');
+
+const button_next = document.getElementById('button-next');
+const button_like = document.getElementById('button-like');
+const button_show = document.getElementById('button-show');
+
+var current: AntiJoke = null;
+
+function random() {
+    let req = new XMLHttpRequest();
+    req.open('GET', `${urlbase}/random/one`, true);
+
+    req.onload = e => {
+        if (req.readyState === 4 && req.status === 200) {
+            current = JSON.parse(req.responseText).items[0] as AntiJoke;
+
+            first_part.textContent = current.first_part;
+            second_part.textContent = current.second_part;
+
+            first_part.style.display = 'block';
+        }
+    };
+
+    req.send(null);
+}
+
+function aleatorio() {
+    button_show.style.display = 'block';
+
+    [first_part, second_part, button_like, button_next].forEach(b => {
+        b.style.display = 'none';
+    });
+
+    random();
+}
+
+// CLICK HANDLERS
+
+function like() {
+    button_like.style.display = 'none';
+
+    let req = new XMLHttpRequest();
+    req.open('POST', `${urlbase}/vote`, true);
+    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    req.send(`id=${current.id}`);
+}
+
+function show() {
+    button_show.style.display = 'none';
+
+    [second_part, button_like, button_next].forEach(b => {
+        b.style.display = 'block';
+    });
+}
+
+/// MAIN
+
+function main() {
+    console.log(`AntiChistes v${version}`);
+    console.log('Mira el código en https://github.com/marionauta/antichistes');
+    aleatorio();
+}
